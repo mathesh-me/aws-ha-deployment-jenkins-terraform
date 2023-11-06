51894024cebdd6fc635ea60e6a53768a298b1e47
mathesh-me <matheshme679@gmail.com>
2023-11-06 22:42:21 +0530

    terraform configuration files are added

---
+++ lauch-template.tf
@@ -0,0 +1,22 @@
+resource "aws_launch_template" "template" {
+  name          = "web-launchtemplate"
+  # ID of the Amazon Machine Image (AMI) to use for the instance
+  image_id      = "ami-05c13eab67c5d8861"
+  instance_type = "t2.micro"
+  key_name = "jenkins-1"
+
+  network_interfaces {
+    device_index = 0
+    security_groups = [aws_security_group.asg_security_group.id]
+  }
+
+  user_data = filebase64("user-data.sh")
+  tag_specifications {
+    
+    resource_type = "instance"
+    tags = {
+      Name = "first"
+    }
+  }
+}
+
